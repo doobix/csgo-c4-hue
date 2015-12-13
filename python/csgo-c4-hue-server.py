ebcb9a4449bd22aa39a5b05fff91bd46e06086b4
Wesley Pascual <doobix@gmail.com>
2015-12-13 11:53:32 -0800

    Use `with` for writing to file

--- python/csgo-c4-hue-server.py
+++ python/csgo-c4-hue-server.py
@@ -6,13 +6,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def main():
-    f = open('bomb_status', 'w')
-    json_data = json.loads(request.data)
-    round_data = json_data.get('round', {})
-    bomb_status = str(round_data.get('bomb', ''))
-    f.write(bomb_status)
-    f.close()
-    print bomb_status
+    with open('bomb_status', 'w') as f:
+        json_data = json.loads(request.data)
+        round_data = json_data.get('round', {})
+        bomb_status = str(round_data.get('bomb', ''))
+        f.write(bomb_status)
+        print bomb_status
     return 'plant the c4!'
 
 if __name__ == "__main__":
